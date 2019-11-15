d6c674912b9a50b8f43c87296a610322db84d1d7
user Ryutta <rzaugusdi354@gmail.com>
2019-11-15 19:47:09 +0700

    insert: api

--- app/src/main/java/com/ryutta/monkingmobile/data/remote/ApiRetrofit.java
+++ app/src/main/java/com/ryutta/monkingmobile/data/remote/ApiRetrofit.java
@@ -1,4 +1,20 @@
 package com.ryutta.monkingmobile.data.remote;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiRetrofit {
+
+    public static final String BASE_URL = "https://monking.com/";
+    private static Retrofit retrofit;
+
+    public static Retrofit getInstance(){
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 }
