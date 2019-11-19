c2a418cca0921dd1cc2e86eff458c376e5b1b916
luluila <ilalulu51@gmail.com>
2019-11-19 20:29:51 +0700

    add:model company

--- app/src/main/java/com/ryutta/monkingmobile/model/Company.java
+++ app/src/main/java/com/ryutta/monkingmobile/model/Company.java
@@ -2,6 +2,9 @@ package com.ryutta.monkingmobile.model;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Company {
     @SerializedName("name")
     private String name;
@@ -27,4 +30,67 @@ public class Company {
     @SerializedName("account")
     private Account[] arrAccount;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(String updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Account[] getArrAccount() {
+        return arrAccount;
+    }
+
+    public void setArrAccount(Account[] arrAccount) {
+        this.arrAccount = arrAccount;
+    }
 }
