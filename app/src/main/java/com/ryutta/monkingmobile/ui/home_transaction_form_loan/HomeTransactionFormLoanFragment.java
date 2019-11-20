ab728d37d89b84db3800f6844d47a7016801e142
user Ryutta <rzaugusdi354@gmail.com>
2019-11-20 09:35:08 +0700

    Insert: Form Transaction Loan Fragment

---
+++ app/src/main/java/com/ryutta/monkingmobile/ui/home_transaction_form_loan/HomeTransactionFormLoanFragment.java
@@ -0,0 +1,33 @@
+package com.ryutta.monkingmobile.ui.home_transaction_form_loan;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ryutta.monkingmobile.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeTransactionFormLoanFragment extends Fragment {
+
+
+    public HomeTransactionFormLoanFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_transaction_form_loan, container, false);
+    }
+
+}
