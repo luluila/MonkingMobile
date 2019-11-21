2c826d6406b1ea007d11d5ec1823bec139d1a746
user Ryutta <rzaugusdi354@gmail.com>
2019-11-21 09:20:21 +0700

    Update HomeFragment.java

--- app/src/main/java/com/ryutta/monkingmobile/ui/home/HomeFragment.java
+++ app/src/main/java/com/ryutta/monkingmobile/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -11,25 +12,47 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.ryutta.monkingmobile.R;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
-    private HomeViewModel homeViewModel;
+    private NavController navController = null;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        view.findViewById(R.id.ib_home_cashIn).setOnClickListener(this);
+        view.findViewById(R.id.ib_home_cashOut).setOnClickListener(this);
+        view.findViewById(R.id.ib_home_debt).setOnClickListener(this);
+        view.findViewById(R.id.ib_home_loan).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.ib_home_cashIn:
+                navController.navigate(R.id.action_navigation_home_to_homeTransactionCashInFragment);
+                break;
+            case R.id.ib_home_cashOut:
+                navController.navigate(R.id.action_navigation_home_to_homeTransactionCashOutFragment);
+                break;
+            case R.id.ib_home_debt:
+                navController.navigate(R.id.action_navigation_home_to_homeTransactionDebtFragment);
+                break;
+            case R.id.ib_home_loan:
+                navController.navigate(R.id.action_navigation_home_to_homeTransactionLoanFragment);
+                break;
+        }
+
     }
 }
