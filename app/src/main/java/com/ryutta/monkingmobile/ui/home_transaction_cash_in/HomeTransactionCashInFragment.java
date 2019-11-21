cc8c8f40b1c94387d0198e3b33bd39577483ee14
user Ryutta <rzaugusdi354@gmail.com>
2019-11-22 03:21:48 +0700

    Update HomeTransactionCashInFragment.java

--- app/src/main/java/com/ryutta/monkingmobile/ui/home_transaction_cash_in/HomeTransactionCashInFragment.java
+++ app/src/main/java/com/ryutta/monkingmobile/ui/home_transaction_cash_in/HomeTransactionCashInFragment.java
@@ -5,23 +5,28 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.ryutta.monkingmobile.R;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeTransactionCashInFragment extends Fragment implements View.OnClickListener{
+public class HomeTransactionCashInFragment extends Fragment implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
 
     private NavController navController = null;
+    private FloatingActionButton floatingActionButton = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,7 +47,34 @@ public class HomeTransactionCashInFragment extends Fragment implements View.OnCl
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.iv_cashIn_option:
-//                navController.navigate(R.id.action);
+                PopupMenu popupMenu = new PopupMenu(view.getContext(), view);
+                popupMenu.setOnMenuItemClickListener(this);
+                popupMenu.inflate(R.menu.transaction_detail_menu);
+                popupMenu.show();
+                break;
+            case R.id.iv_cashIn_back:
+                getActivity().onBackPressed();
+                break;
+        }
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.transaction_detail_current:
+                Toast.makeText(getContext(),"Select: Current", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.transaction_detail_history:
+                Toast.makeText(getContext(),"Select: History", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.transaction_detail_print:
+                Toast.makeText(getContext(),"Select: Print", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.transaction_detail_share:
+                Toast.makeText(getContext(),"Select: Share", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
         }
     }
 }
