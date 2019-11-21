4fb04a08500f7eae1b4c36d3aaa92af526dd623c
user Ryutta <rzaugusdi354@gmail.com>
2019-11-21 09:20:15 +0700

    Update SplashActivity.java

--- app/src/main/java/com/ryutta/monkingmobile/SplashActivity.java
+++ app/src/main/java/com/ryutta/monkingmobile/SplashActivity.java
@@ -13,7 +13,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Start home activity
-        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
 
         // close splash activity
         finish();
