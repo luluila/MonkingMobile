c7c28591ff6ef5f8804be541e63fc260b2912ff3
user Ryutta <rzaugusdi354@gmail.com>
2019-11-22 22:05:08 +0700

    Update MainActivity.java

--- app/src/main/java/com/ryutta/monkingmobile/MainActivity.java
+++ app/src/main/java/com/ryutta/monkingmobile/MainActivity.java
@@ -6,13 +6,11 @@ import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.ryutta.monkingmobile.ui.home_transaction_form_cash_in.HomeTransactionFormCashInFragment;
+import com.ryutta.monkingmobile.ui.home_transaction_form.HomeTransactionFormActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,7 +20,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.all_fab_new);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), HomeTransactionFormActivity.class);
+                startActivity(intent);
+            }
+        });
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
 //      AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -31,8 +36,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
-
     }
 
 }
