991bb81ade75693ee21daf24b10350802481b3b6
user Ryutta <rzaugusdi354@gmail.com>
2019-11-28 10:49:03 +0700

    Update BusinessFragment.java

--- app/src/main/java/com/ryutta/monkingmobile/ui/business/BusinessFragment.java
+++ app/src/main/java/com/ryutta/monkingmobile/ui/business/BusinessFragment.java
@@ -5,12 +5,12 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.PopupMenu;
+import androidx.constraintlayout.widget.Group;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -19,21 +19,30 @@ import com.ryutta.monkingmobile.R;
 
 public class BusinessFragment extends Fragment implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
 
+    Group groupProfitRunning;
+    Group groupProfitHistory;
+    TextView barTitle;
+
     private NavController navController = null;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_business, container, false);
+        View view =  inflater.inflate(R.layout.fragment_business, container, false);
+
+        return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         navController = Navigation.findNavController(view);
+        barTitle = view.findViewById(R.id.tv_business_profit_businessTitle);
+        groupProfitRunning = view.findViewById(R.id.group_profit_running);
+        groupProfitHistory = view.findViewById(R.id.group_profit_history);
+
         view.findViewById(R.id.ib_menu_businessType).setOnClickListener(this);
-//        ImageButton imageButton= (ImageButton) view.findViewById(R.id.ib_menu_businessType);
-//        imageButton.setOnClickListener(this);
+        view.findViewById(R.id.ib_profit_option).setOnClickListener(this);
     }
 
     @Override
@@ -45,6 +54,12 @@ public class BusinessFragment extends Fragment implements View.OnClickListener, 
                 popupMenu.inflate(R.menu.business_type_menu);
                 popupMenu.show();
                 break;
+            case R.id.ib_profit_option:
+                PopupMenu popupMenu2 = new PopupMenu(view.getContext(), view);
+                popupMenu2.setOnMenuItemClickListener(this);
+                popupMenu2.inflate(R.menu.business_detail_profit_menu);
+                popupMenu2.show();
+                break;
         }
     }
 
@@ -52,11 +67,19 @@ public class BusinessFragment extends Fragment implements View.OnClickListener, 
     public boolean onMenuItemClick(MenuItem item) {
         switch (item.getItemId()){
             case R.id.business_type_profit:
-                Toast.makeText(getContext(), "item 1 clicked", Toast.LENGTH_SHORT).show();
-                navController.navigate(R.id.action_navigation_business_to_businessHistoryFragment);
                 return true;
             case R.id.business_type_cost:
-                Toast.makeText(getContext(), "item 2 clicked", Toast.LENGTH_SHORT).show();
+                navController.navigate(R.id.action_navigation_business_to_businessCostFragment);
+                return true;
+            case R.id.business_detail_profit_running:
+                barTitle.setText("PROFIT RUNNING");
+                groupProfitRunning.setVisibility(View.VISIBLE);
+                groupProfitHistory.setVisibility(View.GONE);
+                return true;
+            case R.id.business_detail_profit_history:
+                barTitle.setText("PROFIT HISTORY");
+                groupProfitRunning.setVisibility(View.GONE);
+                groupProfitHistory.setVisibility(View.VISIBLE);
                 return true;
             default:
                 return false;
